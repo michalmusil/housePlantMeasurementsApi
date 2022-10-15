dd559dfae0f92033de0f2b997a61b6f46d6788a5
Michal Musil <michalmusil@Michals-MacBook-Air.local>
2022-10-15 19:27:05 +0200

    Initial commit

---
+++ HousePlantMeasurementsApi/Entities/IDatabaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HousePlantMeasurementsApi.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
+
---
+++ HousePlantMeasurementsApi/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace HousePlantMeasurementsApi.Entities
+{
+    public class User: BaseEntity
+    {
+        
+    }
+}
+
